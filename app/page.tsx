d2819f634d2a25b06825a067fb5dbbce790e5b4d
Emran Hossain <emranffl.biz@gmail.com>
2024-10-10 13:26:45 +0600

    Add .prettierignore file to ignore specific directories and files from Prettier formatting Remove Navbar import from app/page.tsx Add .husky/constants file with terminal output color constants Add .husky/pre-commit file to format code before committing Add .prettierrc file with Prettier configuration settings Update package.json scripts to include clean-install, clean-install-pmy, clean-install-pmb, clean-build, clean-build-pmy, clean-build-pmb, pre-commit, format, and lint Add .husky/pre-push file to check for build errors before pushing to remote repository

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { Contact } from "./Components/Contact";
 import { About } from "./Components/About";
-import { Navbar } from "./Components/Navbar";
 import { Projects } from "./Components/Projects";
 import { Services } from "./Components/Services";
 import { Testimonials } from "./Components/Testimonials";
